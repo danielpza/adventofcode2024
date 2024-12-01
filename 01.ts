ada93b7f057e104689d6a3f94a4153eca6a1847d
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-12-01 04:10:31 -0500

    01

---
+++ 01.ts
@@ -0,0 +1,28 @@
+// $ deno run 01.ts < 01.input
+// https://adventofcode.com/2024/day/1
+
+function problem(input: string) {
+  let arr1: number[] = [];
+  let arr2: number[] = [];
+
+  for (const line of input.trim().split("\n")) {
+    const [a, b] = line.split(/\s+/).map(Number);
+    arr1.push(a);
+    arr2.push(b);
+  }
+
+  arr1 = arr1.sort((a, b) => a - b);
+  arr2 = arr2.sort((a, b) => a - b);
+
+  let sum = 0;
+
+  for (let i = 0; i < arr1.length; i++) {
+    sum += Math.abs(arr1[i] - arr2[i]);
+  }
+
+  return sum;
+}
+
+const input = await new Response(Deno.stdin.readable).text();
+
+console.log(problem(input));
