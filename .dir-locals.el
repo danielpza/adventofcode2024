2aad93677d222aa93847522e618a832729a42ca5
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-12-01 04:09:53 -0500

    deno

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((typescript-ts-mode . ((apheleia-formatter . denofmt)
+						            (lsp-enabled-clients . 'deno-ls))))
